e630206addf3c91e5f3cee9b14a8f8533eb2564c
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-13 13:27:28 -0400

    Create Dataset Load.py

---
+++ Dataset Load.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+# Load the dataset
+data = pd.read_json('https://raw.githubusercontent.com/gunthercox/chatterbot-corpus/master/data/stanford_convertional_ai/stanford_convertional_ai.json')
